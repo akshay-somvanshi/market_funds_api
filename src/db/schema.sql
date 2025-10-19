2039a5313f0314e798d39ae645d069b9106fa36c
akshay-somvanshi <akshaysomvanshi18@gmail.com>
2025-10-19 20:38:59 +0200

    Update db - added dummy rows

--- src/db/schema.sql
+++ src/db/schema.sql
@@ -1,28 +1,31 @@
+-- Enable UUID generator
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 -- Create the Funds table 
 CREATE TABLE fund (
-    id VARCHAR(255) PRIMARY KEY,
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     name VARCHAR(255),
     vintage_year int, 
     target_size_usd NUMERIC(15,2), -- 15 digits with rounding to 2 digits after decimal
     status VARCHAR(255),
-    created_at TIMESTAMPTZ DEFAULT NOW() -- Use TIMESTAMPZ (normalized to UTC)
+    created_at TIMESTAMPTZ DEFAULT NOW(), -- Use TIMESTAMPZ (normalized to UTC)
     updated_at TIMESTAMPTZ DEFAULT NOW()
-)
+);
 
 -- Create the Investors table 
 CREATE TABLE investor (
-    id VARCHAR(255) PRIMARY KEY,
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     name VARCHAR(255),
     investor_type VARCHAR(255),
     email VARCHAR(255) UNIQUE,
     created_at TIMESTAMPTZ DEFAULT NOW()
-)
+);
 
 -- Create the Investments table 
 CREATE TABLE investment (
-    id VARCHAR(255) PRIMARY KEY,
-    investor_id VARCHAR(255) NOT NULL REFERENCES investor(id) ON DELETE CASCADE, -- Automatically delete entry if investor/fund is removed
-    fund_id VARCHAR(255) NOT NULL REFERENCES fund(id) ON DELETE CASCADE,
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    investor_id UUID NOT NULL REFERENCES investor(id) ON DELETE CASCADE, -- Automatically delete entry if investor/fund is removed
+    fund_id UUID NOT NULL REFERENCES fund(id) ON DELETE CASCADE,
     amount_usd NUMERIC(15,2) CHECK (amount_usd >= 0),
     investment_date TIMESTAMPTZ DEFAULT NOW()
-)
+);
---
+++ src/db/seed.sql
@@ -0,0 +1,26 @@
+-- Insert sample funds
+INSERT INTO fund (name, vintage_year, target_size_usd, status)
+VALUES
+('Growth Equity Fund I', 2019, 100000000, 'Active'),
+('Infrastructure Fund II', 2021, 250000000, 'Closed');
+
+-- Insert investors
+INSERT INTO investor(name, investor_type, email)
+VALUES
+('Alice Capital', 'Institutional', 'alice@example.com'),
+('Bob Ventures', 'Individual', 'bob@example.com');
+
+-- Insert sample investments
+INSERT INTO investment(investor_id, fund_id, amount_usd)
+VALUES
+(
+    -- Get Ids from the fund and investor table
+    (SELECT id from investor where name='Alice Capital'),
+    (SELECT id from fund WHERE name='Growth Equity Fund I'), 
+    500000
+),
+(
+    (SELECT id from investor where name='Bob Ventures'),
+    (SELECT id from fund WHERE name='Infrastructure Fund II'), 
+    2000000
+);
